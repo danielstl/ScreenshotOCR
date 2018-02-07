53e6fe5ba654ae60f0e47e6d589df8d2d2186244
danielstl <15856061+danielstl@users.noreply.github.com>
2018-02-07 20:18:08 +0000

    hijacked app bootstrap to add jump list tasks

--- ScreenshotOCR/App.xaml.cs
+++ ScreenshotOCR/App.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Shell;
 
 namespace ScreenshotOCR
 {
@@ -13,5 +15,51 @@ namespace ScreenshotOCR
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var screenshotTask = new JumpTask()
+            {
+                Title = "Take instant screenshot",
+                Description = "Immediately launch screenshot mode",
+                Arguments = "/screenshot",
+                IconResourcePath = Assembly.GetEntryAssembly().CodeBase,
+                ApplicationPath = Assembly.GetEntryAssembly().CodeBase
+            };
+
+            var settingsTask = new JumpTask()
+            {
+                Title = "Settings",
+                Description = "Edit ScreenshotOCR settings",
+                Arguments = "/settings",
+                IconResourcePath = Assembly.GetEntryAssembly().CodeBase,
+                ApplicationPath = Assembly.GetEntryAssembly().CodeBase
+            };
+
+            var jumpList = new JumpList();
+            jumpList.JumpItems.Add(screenshotTask);
+            jumpList.JumpItems.Add(settingsTask);
+            jumpList.ShowFrequentCategory = false;
+            jumpList.ShowRecentCategory = false;
+
+            JumpList.SetJumpList(Current, jumpList);
+
+            if (e.Args.Length > 0)
+            {
+                if (e.Args[0] == "/screenshot")
+                {
+                    new ScreenshotTaker().Show();
+                    return;
+                } else if (e.Args[0] == "/settings")
+                {
+                    //todo
+                    MessageBox.Show("settings coming soon!");
+                    //return;
+                }
+            }
+
+            new MainWindow().Show();
+        }
     }
 }
